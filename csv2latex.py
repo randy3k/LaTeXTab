3ab6b77d03a56e07d7d77354d7f8f5431bf4f001
Randy Lai <randy.cs.lai@gmail.com>
2013-10-25 00:17:08 -0700

    set point to 0 if sel() is empty

--- csv2latex.py
+++ csv2latex.py
@@ -70,10 +70,10 @@ class CsvToTableCommand(sublime_plugin.TextCommand):
 
     def is_enabled(self):
         view = self.view
-        point = view.sel()[0].end()
+        point = view.sel()[0].end() if view.sel() else 0
         return view.score_selector(point, "text.tex.latex")>0
 
     def is_visible(self):
         view = self.view
-        point = view.sel()[0].end()
+        point = view.sel()[0].end() if view.sel() else 0
         return view.score_selector(point, "text.tex.latex")>0
